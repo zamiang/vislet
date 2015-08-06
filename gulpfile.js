b9d974d847bedf3382d1c5204ebafb8b9d346472
Brennan Moore <brennanmoore@gmail.com>
2015-08-06 15:46:15 -0400

    cleanup gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -95,60 +95,66 @@ gulp.task("images", function() {
     .pipe(gulp.dest("./dist/img"));
 });
 
-gulp.task("compress", function() {
+gulp.task("compress", ['scripts'], function(cb) {
   gulp.src("./dist/*/*.js")
     .pipe(uglify())
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./dist"))
+    .on('end', cb);
 });
 
 // TODO: Combine publish tasks
-gulp.task("publish-html", ['default', 'compress'], function() {
+gulp.task("publish-html", ['default', 'compress'], function(cb) {
   var options = {
     headers: {
       "Cache-Control": defaultCacheControl,
+      'charset': 'utf-8',
       'Content-Type': 'text/html'
     }};
 
-  gulp.src("./dist/*/*.html")
-    .pipe(s3(aws, options));
+  gulp.src("./dist/**/*.html")
+    .pipe(s3(aws, options))
+    .on('end', cb);
 });
 
-gulp.task("publish-images", ['default', 'compress'], function() {
+gulp.task("publish-images", ['default', 'compress'], function(cb) {
   var options = {
     headers: {
       "Cache-Control": defaultCacheControl
     }};
 
   gulp.src(["./dist/*/*.png","./dist/**/*.jpg"])
-    .pipe(s3(aws, options));
+    .pipe(s3(aws, options))
+    .on('end', cb);
 });
 
-gulp.task("publish-scripts", ['default', 'compress'], function() {
+gulp.task("publish-scripts", ['default', 'compress'], function(cb) {
   var options = {
     headers: {
       "Cache-Control": defaultCacheControl,
-      'Content-Type': 'application/javascript'
+      'Content-Encoding': 'gzip',
+      'Content-Type': 'application/javascript',
+      'charset': 'utf-8'
     }};
-
-  options.headers['Content-Type'] = 'application/javascript';
-  options.headers['Content-Encoding'] = 'gzip';
 
   gulp.src("./dist/*/*.js")
     .pipe(gzip({ append: false }))
-    .pipe(s3(aws, options));
+    .pipe(s3(aws, options))
+    .on('end', cb);
 });
 
-gulp.task("publish-styles", ['default', 'compress'], function() {
+gulp.task("publish-styles", ['default', 'compress'], function(cb) {
   var options = {
     headers: {
       "Cache-Control": defaultCacheControl,
+      'Content-Encoding': 'gzip',
       'Content-Type': 'text/css',
       'charset': 'utf-8'
     }};
 
   gulp.src("./dist/*/*.css")
     .pipe(gzip({ append: false }))
-    .pipe(s3(aws, options));
+    .pipe(s3(aws, options))
+    .on('end', cb);
 });
 
 gulp.task("watch", function() {
@@ -158,4 +164,4 @@ gulp.task("watch", function() {
 });
 
 gulp.task("default", ["scripts", "styles", "images", "templates"]);
-gulp.task("publish", ["default", "publish-html", "publish-scripts", "publish-styles", "publish-images"]);
+gulp.task("publish", ["publish-html", "publish-scripts", "publish-styles", "publish-images"]);
